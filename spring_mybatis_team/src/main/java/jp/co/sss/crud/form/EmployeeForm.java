673dce978dace6b9b7afe878b57358449b274258
t-mizoguchi <t.mizoguchi.aivan@gmail.com>
2025-12-23 16:25:28 +0900

    fix:溝口 社員変更機能の追加

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/form/EmployeeForm.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/form/EmployeeForm.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 
@@ -14,6 +15,7 @@ public class EmployeeForm {
 
     /** パスワード */
     @NotBlank
+    @Pattern(regexp = "^\\d+$")
     @Size(min = 1, max = 16)
     private String empPass;
 
